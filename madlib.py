6ea4ec3541ae58de2380e552654bd988b9d2bee5
Bartek <bartekaleks.hubisz@gmail.com>
2023-06-05 15:42:01 +0200

    first ever commmit - working version

---
+++ madlib.py
@@ -0,0 +1,7 @@
+noob = input("Your name: ")
+pro = input("your dream job: ")
+hacker = input("what you love to do: ")
+
+madlib = f"my name is {noob}. when i grow up i want to be {pro}. i want to be {pro} because i love {hacker}."
+
+print (madlib)
